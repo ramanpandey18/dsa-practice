076c78a26af27e9d9b8fb1b14ac8445f55da571b
Raman Pandey <ramanpandey1807@gmail.com>
2026-05-05 13:56:45 +0530

    05-05-2026-13:56

---
+++ sliding_window/sell_stock.py
@@ -0,0 +1,28 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        arrLen = len(prices)
+        if arrLen <= 0:
+            return 0
+        buyDay = 0
+        sellDay = 0
+        maxProfit = 0
+        for i in range(arrLen - 1):
+            profit = prices[i+1] - prices[buyDay]
+            print("profit", profit)
+            if profit <= 0:
+                buyDay  = i + 1 
+                # sellDay = i
+            if profit > maxProfit:
+                sellDay = i + 1
+                maxProfit = profit
+            print("sellDay", sellDay)
+            print("buyDay", buyDay)
+            print("maxProfit", maxProfit)
+        return maxProfit
+
+
+solution = Solution()
+prices=[7,1,5,3,6,4]
+result = solution.maxProfit(prices)
+print("Output:", result)
